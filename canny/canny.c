fc58fdfafcde8c832ceee04d407af724d69200cc
nick <nick@pop-os.localdomain>
2021-01-25 10:06:38 -0500

    finished

--- canny/canny.c
+++ canny/canny.c
@@ -1,144 +1,305 @@
 #include <stdio.h>                  /*  Marr-Hildreth.c  (or marrh.c) */
 #include <math.h>
-#include <stdlib.h>
-
 #define  PICSIZE 256
 #define  MAXMASK 100
 
-         int    pic[PICSIZE][PICSIZE];
-         double outpic1[PICSIZE][PICSIZE];
-         double outpic2[PICSIZE][PICSIZE];
-         int    edgeflag[PICSIZE][PICSIZE];
-         double mask[MAXMASK][MAXMASK];
-         double maskx[MAXMASK][MAXMASK];
-	 double masky[MAXMASK][MAXMASK];
-	 double conv[PICSIZE][PICSIZE];
+int    pic[PICSIZE][PICSIZE];
+double outpicx[PICSIZE][PICSIZE];
+double outpicy[PICSIZE][PICSIZE];
+int    edgeflag[PICSIZE][PICSIZE];
+double mask[MAXMASK][MAXMASK];
+double mag[PICSIZE][PICSIZE];
+double finals[PICSIZE][PICSIZE];
+double peaks[PICSIZE][PICSIZE];
+double ival[256][256];
+double maskx[MAXMASK][MAXMASK];
+double masky[MAXMASK][MAXMASK];
+int histo[PICSIZE];
 
-void main(argc,argv)
+int main(argc,argv)
 int argc;
 char **argv;
 {
-	/* TODO:
-	 x marrh.c uses second derivatives of gauss, use first derivatives instead
-	 x include partial derivative in respect to y
-	 * keep the convolution code, but double up on convolution for x and y
-	 * delete everything below the convolution code
-	 * go to sobel code, bring in the sqrt of squares code and the scaling of the output and maxival
-	 */
-	double e = 2.71828;
 
-	int     i,j,p,q,s,t,mr,centx,centy;
-        double  maskval,sum,sig,maxival,minival,maxval,ZEROTOL;
-        FILE    *fo1, *fo2,*fp1, *fopen();
-        char    *foobar;
+	int i,j,p,q,s,t,mr,centx,centy, more;
+	double  maskval,sum1,sum2,sig,maxival,minival,maxval,ZEROTOL, dx, dy, slope, HI, LO, percent, cutOff, topsArea;
+	FILE    *fo1, *fo2, *fo3, *fp1, *fopen();
+	char *foobar;
+	// Opening and closing files for reading and writing
+	// file names are taken from cmd prompt
 
-        argc--; argv++;
-        foobar = *argv;
-        printf("%s\n", foobar);
+	/* input file */
+	argc--; argv++;
+	foobar = *argv;
 	fp1=fopen(foobar,"rb");
 
-        argc--; argv++;
-        foobar = *argv;
-        printf("%s\n", foobar);
+	// convolution output image
+	foobar = "output/convolution.pgm";
 	fo1=fopen(foobar,"wb");
+	fprintf(fo1,"P5\n257 257\n255\n");
 
-        argc--; argv++;
-        foobar = *argv;
-        sig = atof(foobar);
+	
+	// peaks output image
+	foobar = "output/peaks.pgm";
+	fo2=fopen(foobar,"wb");
+	fprintf(fo2,"P5\n257 257\n255\n");
 
-	int rows, cols = 256;
-	/* write PGM header */
-    	fprintf(fo1, "P5\n256 256\n255\n", rows, cols);
+	// edge detection image
+	foobar = "output/edges.pgm";
+	fo3=fopen(foobar,"wb"); 
+	fprintf(fo3,"P5\n257 257\n255\n");
 
+	slope = 0.0;
+	sig = 1.0;
+	mr = (int)(sig * 3);
 
-        mr = (int)(sig * 3);
         centx = (MAXMASK / 2);
         centy = (MAXMASK / 2);
 
+        // read input picture into memory
         for (i=0;i<256;i++)
-        { for (j=0;j<256;j++)
+        {
+        	for (j=0;j<256;j++)
                 {
-                  pic[i][j]  =  getc (fp1);
-                  //printf("%c\n", pic[i][j]);
-		}
-        }
-	
-	// probably need an x mask and y mask, the convolve each
-	
-        for (p=-mr;p<=mr;p++)
-        {  for (q=-mr;q<=mr;q++)
-           {
-	      /* change this equation */
-              //maskval = ((2-(((p*p)+(q*q))/(sig*sig))) * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
-              
-	      // p = y, q = x
-	      maskval = (q) * exp(((-1*q*q)+(p*p))/(2*sig*sig));
-	      (maskx[p+centy][q+centx]) = maskval;
-           }
-        }
-	
-       	for (p=-mr;p<=mr;p++)
-        {  for (q=-mr;q<=mr;q++)
-           {
-	      /* change this equation */
-              //maskval = ((2-(((p*p)+(q*q))/(sig*sig))) * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
-              
-	      // p = y, q = x
-	      
-	      maskval += (p) * exp(((-1*q*q)+(p*p))/(2*sig*sig));
-	      (masky[p+centy][q+centx]) = maskval;
-           }
+                    pic[i][j]  =  getc (fp1);
+                }
         }
 
-	/* compute convolutions */
+        for (p=-mr;p<=mr;p++)
+        {
+            for (q=-mr;q<=mr;q++)
+            {
+                // create x & y masks from gaussian partial derivatives
+                dx = q * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
+                maskx[p+centy][q+centx] = dx;
+
+		dy = p * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
+                masky[p+centy][q+centx] = dy;
+            }
+        }
+
+        // compute convolution
         for (i=mr;i<=255-mr;i++)
-        { for (j=mr;j<=255-mr;j++)
-          {
-             sum = 0;
-             for (p=-mr;p<=mr;p++)
-             {
-                for (q=-mr;q<=mr;q++)
+        {
+            for (j=mr;j<=255-mr;j++)
+            {
+                sum1 = 0;
+                sum2 = 0;
+
+                for (p=-mr;p<=mr;p++)
                 {
-                   sum += pic[i+p][j+q] * maskx[p+centy][q+centx];
-		   sum += pic[i+p][j+q] * masky[p+centy][q+centx];
+                    for (q=-mr;q<=mr;q++)
+                    {
+                        sum1 += pic[i+p][j+q] * maskx[p+centy][q+centx];
+                        sum2 += pic[i+p][j+q] * masky[p+centy][q+centx];
+                    }
+             }
+
+             outpicx[i][j] = sum1;
+             outpicy[i][j] = sum2;
+
+            }
+        }
+	
+
+        maxival = 0;
+        for (i=mr;i<256-mr;i++)
+        { 
+            for (j=mr;j<256-mr;j++)
+            {
+		// take magnitude of each convolution output
+                ival[i][j]=sqrt((double)((outpicx[i][j]*outpicx[i][j]) +
+                                        (outpicy[i][j]*outpicy[i][j])));
+
+             if (ival[i][j] > maxival)
+                maxival = ival[i][j];
+
+             }
+        }
+
+	// output convolution image
+        for (i=0;i<256;i++)
+        {
+            for (j=0;j<256;j++)
+            {
+                mag[i][j] = ival[i][j];
+                ival[i][j] = (ival[i][j]/maxival) * 255;
+                fprintf(fo1,"%c",(char)((int)(ival[i][j])));
+            }
+            
+            fprintf(fo1,"\n");
+          }
+
+	// ??? need explaination
+	// compute peaks
+        for (i=mr;i<256-mr;i++)
+        {
+            for (j=mr;j<256-mr;j++)
+            {
+                if(outpicx[i][j] == 0.0)
+                {
+                    outpicx[i][j] == 0.00001;
+                }
+
+            slope = (double)(outpicy[i][j])/(double)(outpicx[i][j]);
+
+            if((slope <= 0.4142) && (slope > -0.4142))
+            {
+                if((ival[i][j] > ival[i][j-1]) && (ival[i][j] > ival[i][j+1]))
+                {
+                    peaks[i][j] = 255;
+                }
+            }
+            else if((slope <= 2.4142) && (slope > 0.4142))
+            {
+                if((ival[i][j] > ival[i-1][j-1]) && (ival[i][j] > ival[i+1][j+1]))
+                {
+                    peaks[i][j] = 255;
 
                 }
-             }
-             outpic1[i][j] = sum;
-             conv[i][j] = sum;
+            }
+            else if((slope <= -0.4142) && (slope > -2.4142))
+            {
+                if((ival[i][j] > ival[i+1][j-1]) && (ival[i][j] > ival[i-1][j+1]))
+                {
+                    peaks[i][j] = 255;
+
+                }
+            }
+            else
+            {
+                if((ival[i][j] > ival[i-1][j]) && (ival[i][j] > ival[i+1][j]))
+                {
+                    peaks[i][j] = 255;
+                }
+            }
+
+           }
+        }
+
+	// find maxival for peaks image
+        maxival = 0;
+        for (i=mr;i<256-mr;i++)
+        { for (j=mr;j<256-mr;j++)
+          {
+
+              if (peaks[i][j] > maxival)
+                maxival = peaks[i][j];
+           }
+        }
+	
+	// output peaks image
+        for (i=0;i<256;i++)
+          { for (j=0;j<256;j++)
+            {
+                peaks[i][j] = (peaks[i][j]/maxival) * 255;
+                fprintf(fo2,"%c",(char)((int)(peaks[i][j])));
+            }
+            fprintf(fo2,"\n");
           }
-	}
 
-	// lets output the image and see what happens
-	int diff;
-    	maxival = 0;
-    	for (i=mr;i<256-mr;i++)
-   	{
-		for (j=mr;j<256-mr;j++)
-		{
-	    		// magnitude vector
-	    		//diff = sqrt((double)((outpicx[i][j]*outpicx[i][j]) + (outpicy[i][j]*outpicy[i][j])));
-	    
-	    		// is the magnitude of the jump big enough?            
-	    		//ival[i][j] = diff > threshold && diff < hiThreshold ? 1 : 0;
-    
-	    		if (outpic1[i][j] > maxival)
-				maxival = outpic1[i][j];
+        /* double thresholding */
+        percent = 0.05;
+        topsArea = 0;
 
-		}
-    	}
- 
+        // histogram
+        for (i=mr;i<256-mr;i++)
+        {
+            for (j=mr;j<256-mr;j++)
+            {
+                histo[(int)(ival[i][j])]++;
+            }
+        }
 
-    // normalize pixels and output image
-    for (i=0;i<256;i++)
-    {
-	for (j=0;j<256;j++)
-	{
-	     pic[i][j] = (pic[i][j] / maxival) * 255;            
-	     fprintf(fo1,"%c",(char)((int)(pic[i][j])));
-	     
-	}
-    }
+        cutOff = percent * 255 * 255;
+	
+	// find threshold
+        for (i = 255; i > 0; i--)
+        {
+            topsArea += histo[i];
+            if(topsArea > cutOff)
+                break;
+        }
 
+        HI = i;
+        LO = 0.35 * HI;
+	
+	// output thresholds
+        printf("HI = %lf, LO = %lf \n",HI, LO);
+
+        more = 0;
+
+	// find edge values above HI
+        for (i=0;i<256;i++)
+        {
+            for (j=0;j<256;j++)
+            {
+                if(peaks[i][j] > 254)
+                {
+                    if(ival[i][j] > HI)
+                    {
+                        peaks[i][j] = 0;
+                        finals[i][j] = 255;
+                    }
+                    else if(ival[i][j] < LO)
+                    {
+                        peaks[i][j] = 0;
+                        finals[i][j] = 0;
+                    }
+                }
+            }
+         }
+
+	// ??? find mid values
+        more = 1;
+        while(more == 1)
+        {
+            more = 0;
+            for (i=0;i<256;i++)
+            {
+                for (j=0;j<256;j++)
+                {
+                    if(peaks[i][j] > 254)
+                    {
+                        for (p=-mr;p<=mr;p++)
+                        {
+                            for (q=-mr;q<=mr;q++)
+                            {
+                                if(finals[i+p][j+q] > 254)
+                                {
+                                    peaks[i][j] = 0;
+                                    finals[i][j] = 255;
+                                    more = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // find maxival for normalization
+        maxival = 0;
+        for (i=mr;i<256-mr;i++)
+        {
+            for (j=mr;j<256-mr;j++)
+            {
+
+                if (finals[i][j] > maxival)
+                    maxival = finals[i][j];
+
+           }
+        }
+        
+        // normalize pixels and write image
+        for (i=0;i<256;i++)
+        {
+            for (j=0;j<256;j++)
+            {
+                finals[i][j] = (finals[i][j]/maxival) * 255;
+                fprintf(fo3,"%c",(char)((int)(finals[i][j])));
+            }
+            
+            fprintf(fo3,"\n");
+         }
 }
-
--- canny/sobel.c
+++ canny/sobel.c
@@ -1,136 +1,305 @@
-/* Sobel.c */
-#include <stdio.h>
+#include <stdio.h>                  /*  Marr-Hildreth.c  (or marrh.c) */
 #include <math.h>
-#include <stdlib.h>
-#include <limits.h>
+#define  PICSIZE 256
+#define  MAXMASK 100
 
-int pic[256][256];
-int outpicx[256][256];
-int outpicy[256][256];
+int    pic[PICSIZE][PICSIZE];
+double outpicx[PICSIZE][PICSIZE];
+double outpicy[PICSIZE][PICSIZE];
+int    edgeflag[PICSIZE][PICSIZE];
+double mask[MAXMASK][MAXMASK];
+double mag[PICSIZE][PICSIZE];
+double finals[PICSIZE][PICSIZE];
+double peaks[PICSIZE][PICSIZE];
+double ival[256][256];
+double maskx[MAXMASK][MAXMASK];
+double masky[MAXMASK][MAXMASK];
+int histo[PICSIZE];
 
-/* convolution masks */
-int maskx[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}};
-int masky[3][3] = {{1,2,1},{0,0,0},{-1,-2,-1}};
-double ival[256][256],maxival;
-
-int main(argc, argv)
+int main(argc,argv)
 int argc;
 char **argv;
 {
-    int i,j,p,q,mr,sum1,sum2;
-    double threshold;        // lo threshold
-    double hiThreshold = 100;
 
-    FILE *fo1, *fo2, *fp1, *fopen();
-    char *foobar;
-       
-    int magnitude = 0;
+	int i,j,p,q,s,t,mr,centx,centy, more;
+	double  maskval,sum1,sum2,sig,maxival,minival,maxval,ZEROTOL, dx, dy, slope, HI, LO, percent, cutOff, topsArea;
+	FILE    *fo1, *fo2, *fo3, *fp1, *fopen();
+	char *foobar;
+	// Opening and closing files for reading and writing
+	// file names are taken from cmd prompt
 
-    /* input file */
-    argc--; argv++;
-    foobar = *argv;
-    fp1=fopen(foobar,"rb");
-
-    /* output file */
+	/* input file */
 	argc--; argv++;
 	foobar = *argv;
+	fp1=fopen(foobar,"rb");
+
+	// convolution output image
+	foobar = "output/convolution.pgm";
 	fo1=fopen(foobar,"wb");
-    
-    int rows = 256;
-    int cols = 256;
-    
-    /* write PGM header */
-    fprintf(fo1, "P5\n%d %d\n255\n", rows, cols);
-        
-    argc--; argv++;
-	foobar = *argv;
+	fprintf(fo1,"P5\n257 257\n255\n");
 
-	/* threshold value */
-    threshold = atof(foobar);
+	
+	// peaks output image
+	foobar = "output/peaks.pgm";
+	fo2=fopen(foobar,"wb");
+	fprintf(fo2,"P5\n257 257\n255\n");
 
-    // scan in (optional) Hi Threshold
-    argc--; argv++;
-    foobar = *argv;
-    
-       
-    if (foobar == NULL)
-    {
-        // set hiThreshold arbitrarily high if no input specified
-        hiThreshold = INT_MAX;
-    }
-    else
-    {
-         hiThreshold = atof(foobar);
-    }
- 
-    /* scan in image */
-    for (i=0;i<256;i++)
-    {
-        for (j=0;j<256;j++)
+	// edge detection image
+	foobar = "output/edges.pgm";
+	fo3=fopen(foobar,"wb"); 
+	fprintf(fo3,"P5\n257 257\n255\n");
+
+	slope = 0.0;
+	sig = 1.0;
+	mr = (int)(sig * 3);
+
+        centx = (MAXMASK / 2);
+        centy = (MAXMASK / 2);
+
+        // read input picture into memory
+        for (i=0;i<256;i++)
         {
-            pic[i][j]  =  getc (fp1);
-
-            /* legacy octal eight 1s bitmask for reading chars */
-            pic[i][j]  &= 0377;
-
-            magnitude += pic[i][j];
-        }
-    }
-    
-    /* convolution */
-    
-    mr = 1; //  mask radius
-
-    for (i=mr;i<256-mr;i++)
-    {
-        for (j=mr;j<256-mr;j++)
-        {
-            sum1 = 0;
-            sum2 = 0;
-            for (p=-mr;p<=mr;p++)
-            { 
-                for (q=-mr;q<=mr;q++)
+        	for (j=0;j<256;j++)
                 {
-                    sum1 += pic[i+p][j+q] * maskx[p+mr][q+mr];
-                    sum2 += pic[i+p][j+q] * masky[p+mr][q+mr];
+                    pic[i][j]  =  getc (fp1);
                 }
-            }
-            
-            outpicx[i][j] = sum1;
-            outpicy[i][j] = sum2;
         }
-    }
-    
-    int diff;
-    maxival = 0;
-    for (i=mr;i<256-mr;i++)
-    {
-        for (j=mr;j<256-mr;j++)
+
+        for (p=-mr;p<=mr;p++)
         {
-            // magnitude vector
-            diff = sqrt((double)((outpicx[i][j]*outpicx[i][j]) + (outpicy[i][j]*outpicy[i][j])));
-            
-            // is the magnitude of the jump big enough?            
-            ival[i][j] = diff > threshold && diff < hiThreshold ? 1 : 0;
-    
-            if (ival[i][j] > maxival)
+            for (q=-mr;q<=mr;q++)
+            {
+                // create x & y masks from gaussian partial derivatives
+                dx = q * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
+                maskx[p+centy][q+centx] = dx;
+
+		dy = p * (exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
+                masky[p+centy][q+centx] = dy;
+            }
+        }
+
+        // compute convolution
+        for (i=mr;i<=255-mr;i++)
+        {
+            for (j=mr;j<=255-mr;j++)
+            {
+                sum1 = 0;
+                sum2 = 0;
+
+                for (p=-mr;p<=mr;p++)
+                {
+                    for (q=-mr;q<=mr;q++)
+                    {
+                        sum1 += pic[i+p][j+q] * maskx[p+centy][q+centx];
+                        sum2 += pic[i+p][j+q] * masky[p+centy][q+centx];
+                    }
+             }
+
+             outpicx[i][j] = sum1;
+             outpicy[i][j] = sum2;
+
+            }
+        }
+	
+
+        maxival = 0;
+        for (i=mr;i<256-mr;i++)
+        { 
+            for (j=mr;j<256-mr;j++)
+            {
+		// take magnitude of each convolution output
+                ival[i][j]=sqrt((double)((outpicx[i][j]*outpicx[i][j]) +
+                                        (outpicy[i][j]*outpicy[i][j])));
+
+             if (ival[i][j] > maxival)
                 maxival = ival[i][j];
 
+             }
         }
-    }
-    
-    // normalize pixels and output image
-    for (i=0;i<256;i++)
-    {
-        for (j=0;j<256;j++)
+
+	// output convolution image
+        for (i=0;i<256;i++)
         {
-             ival[i][j] = (ival[i][j] / maxival) * 255;            
-             fprintf(fo1,"%c",(char)((int)(ival[i][j])));
-             
+            for (j=0;j<256;j++)
+            {
+                mag[i][j] = ival[i][j];
+                ival[i][j] = (ival[i][j]/maxival) * 255;
+                fprintf(fo1,"%c",(char)((int)(ival[i][j])));
+            }
+            
+            fprintf(fo1,"\n");
+          }
+
+	// ??? need explaination
+	// compute peaks
+        for (i=mr;i<256-mr;i++)
+        {
+            for (j=mr;j<256-mr;j++)
+            {
+                if(outpicx[i][j] == 0.0)
+                {
+                    outpicx[i][j] == 0.00001;
+                }
+
+            slope = (double)(outpicy[i][j])/(double)(outpicx[i][j]);
+
+            if((slope <= 0.4142) && (slope > -0.4142))
+            {
+                if((ival[i][j] > ival[i][j-1]) && (ival[i][j] > ival[i][j+1]))
+                {
+                    peaks[i][j] = 255;
+                }
+            }
+            else if((slope <= 2.4142) && (slope > 0.4142))
+            {
+                if((ival[i][j] > ival[i-1][j-1]) && (ival[i][j] > ival[i+1][j+1]))
+                {
+                    peaks[i][j] = 255;
+
+                }
+            }
+            else if((slope <= -0.4142) && (slope > -2.4142))
+            {
+                if((ival[i][j] > ival[i+1][j-1]) && (ival[i][j] > ival[i-1][j+1]))
+                {
+                    peaks[i][j] = 255;
+
+                }
+            }
+            else
+            {
+                if((ival[i][j] > ival[i-1][j]) && (ival[i][j] > ival[i+1][j]))
+                {
+                    peaks[i][j] = 255;
+                }
+            }
+
+           }
         }
-    }
-    
-    printf("Magnitude: %d\n", magnitude);
-    printf("Hi Threshold: %.2f\n", hiThreshold);
-    printf("Lo Threshold: %.2f\n", threshold);
+
+	// find maxival for peaks image
+        maxival = 0;
+        for (i=mr;i<256-mr;i++)
+        { for (j=mr;j<256-mr;j++)
+          {
+
+              if (peaks[i][j] > maxival)
+                maxival = peaks[i][j];
+           }
+        }
+	
+	// output peaks image
+        for (i=0;i<256;i++)
+          { for (j=0;j<256;j++)
+            {
+                peaks[i][j] = (peaks[i][j]/maxival) * 255;
+                fprintf(fo2,"%c",(char)((int)(peaks[i][j])));
+            }
+            fprintf(fo2,"\n");
+          }
+
+        /* double thresholding */
+        percent = 0.05;
+        topsArea = 0;
+
+        // histogram
+        for (i=mr;i<256-mr;i++)
+        {
+            for (j=mr;j<256-mr;j++)
+            {
+                histo[(int)(ival[i][j])]++;
+            }
+        }
+
+        cutOff = percent * 255 * 255;
+	
+	// find threshold
+        for (i = 255; i > 0; i--)
+        {
+            topsArea += histo[i];
+            if(topsArea > cutOff)
+                break;
+        }
+
+        HI = i;
+        LO = 0.35 * HI;
+	
+	// output thresholds
+        printf("HI = %lf, LO = %lf \n",HI, LO);
+
+        more = 0;
+
+	// find edge values above HI
+        for (i=0;i<256;i++)
+        {
+            for (j=0;j<256;j++)
+            {
+                if(peaks[i][j] > 254)
+                {
+                    if(ival[i][j] > HI)
+                    {
+                        peaks[i][j] = 0;
+                        finals[i][j] = 255;
+                    }
+                    else if(ival[i][j] < LO)
+                    {
+                        peaks[i][j] = 0;
+                        finals[i][j] = 0;
+                    }
+                }
+            }
+         }
+
+	// ??? find mid values
+        more = 1;
+        while(more == 1)
+        {
+            more = 0;
+            for (i=0;i<256;i++)
+            {
+                for (j=0;j<256;j++)
+                {
+                    if(peaks[i][j] > 254)
+                    {
+                        for (p=-mr;p<=mr;p++)
+                        {
+                            for (q=-mr;q<=mr;q++)
+                            {
+                                if(finals[i+p][j+q] > 254)
+                                {
+                                    peaks[i][j] = 0;
+                                    finals[i][j] = 255;
+                                    more = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // find maxival for normalization
+        maxival = 0;
+        for (i=mr;i<256-mr;i++)
+        {
+            for (j=mr;j<256-mr;j++)
+            {
+
+                if (finals[i][j] > maxival)
+                    maxival = finals[i][j];
+
+           }
+        }
+        
+        // normalize pixels and write image
+        for (i=0;i<256;i++)
+        {
+            for (j=0;j<256;j++)
+            {
+                finals[i][j] = (finals[i][j]/maxival) * 255;
+                fprintf(fo3,"%c",(char)((int)(finals[i][j])));
+            }
+            
+            fprintf(fo3,"\n");
+         }
 }
